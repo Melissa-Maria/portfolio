43a6936b1cc99a097eed4b3ce7cc8e9c1dd478b1
Melissa Pignalosa <m.pignalosa@hotmail.com>
2024-08-19 14:18:02 +0930

    Edited projects section with live sites

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* document.body.addEventListener('mouseover', e => {
+    let hover = e.target.closest('.hover');
+    if (!hover) { return; }
+    hover.style.backgroundColor = '#B7ADCF';
+  });
+  
+  document.body.addEventListener('mouseout', e => {
+    let hover = e.target.closest('.hover');
+    if (!hover) { return; }
+    hover.style.backgroundColor = '';
+  }); */
+
+/* var image = document.getElementByClass('hover');
+
+image.addEventListener('mouseover', function() { 
+    image.setAttribute('style','-webkit-filter: brightness(2)'); 
+}, false);
+
+image.addEventListener('mouseout', function() { 
+    image.setAttribute('style','-webkit-filter: brightness(1.0)'); 
+}, false); */
